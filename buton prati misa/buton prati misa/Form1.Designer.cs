2869ae2562b38575a46128c768dc4beff3a3674c
mzupanic2 <mzupanic2@foi.hr>
2015-04-01 08:22:40 +0200

    Završeno sljeđenje pointera
    Dovršena je funkcija koja slijedi pointer na njegovom putu prema nečemu
    :P

---
+++ buton prati misa/buton prati misa/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace buton_prati_misa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrati = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrati
+            // 
+            this.btnPrati.Location = new System.Drawing.Point(94, 86);
+            this.btnPrati.Name = "btnPrati";
+            this.btnPrati.Size = new System.Drawing.Size(75, 23);
+            this.btnPrati.TabIndex = 0;
+            this.btnPrati.Text = "Pratim te ;)";
+            this.btnPrati.UseVisualStyleBackColor = true;
+            this.btnPrati.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnPrati_MouseClick);
+            this.btnPrati.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnPrati_MouseDown);
+            this.btnPrati.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnPrati_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnPrati);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrati;
+    }
+}
+
---
+++ buton prati misa/buton prati misa/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buton_prati_misa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrati_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            btnPrati.Location = PointToClient(Cursor.Position);
+
+        }
+
+        private void btnPrati_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnPrati.Location = PointToClient(Cursor.Position);
+        }
+
+        private void btnPrati_MouseClick(object sender, MouseEventArgs e)
+        {
+            Random z = new Random();
+            int v = z.Next(-30,50);
+            int u = z.Next(-50, 50);
+
+            Size l = new Size(v, u);
+
+            btnPrati.Location = Point.Add(btnPrati.Location, l);
+
+        }
+    }
+}
